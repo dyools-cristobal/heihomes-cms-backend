c3d9bce5c0f37c53f70e2be05eb58d86c9e79ef8
dyools-cristobal <78533777+dyools-cristobal@users.noreply.github.com>
2024-08-26 21:48:31 +0800

    CORS update

--- main.py
+++ main.py
@@ -34,6 +34,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST", "GET", "PUT", "DELETE"],
     allow_headers=["*"]
 )
